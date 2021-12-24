03ad854f7ceae05ffd6eb4550fe7d00637cb2b13
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-12-24 21:05:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from hdt import HDTDocument, IdentifierPosition
+
+
+
+
+files = [fibo-skos, fibo-owl,]
+
+
+for file in files:
+	path_to_file = './'+file+'.hdt'
+	hdt_metalink = HDTDocument(PATH_META)
