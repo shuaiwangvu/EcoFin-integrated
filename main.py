ad6bfa75a4f4b43f44f8cab93669c2bf18d991a2
shuaiwangvu <shuai.wang.vu@gmail.com>
2021-12-28 23:43:17 +0100

    update main

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ skos_narrowMatch = "http://www.w3.org/2004/02/skos/core#narrowMatch"
 skos_narrower = "http://www.w3.org/2004/02/skos/core#narrower"
 skos_relatedMatch = "http://www.w3.org/2004/02/skos/core#relatedMatch"
 skos_closeMatch = "http://www.w3.org/2004/02/skos/core#closeMatch"
-
-files = ['fibo-skos', 'fibo-owl', 'fro', 'hfr', 'lkif', 'bro', 'figi', 'stw', 'stw-mappings', 'jel']
+lkif_eq = 'http://www.estrellaproject.org/lkif-core/norm.owl#strictly_equivalent'
+files = ['fibo-skos', 'fibo-owl', 'fro', 'hfr', 'lkif', 'bro', 'figi', 'stw', 'stw-mappings', 'jel', 'fund']
 # files = []
 
 for file in files:
@@ -60,7 +60,9 @@ for file in files:
 	triples, cardinality = hdt_kg.search_triples("", skos_relatedMatch, "")
 	print ('skos:relatedMatch ', cardinality)
 
-
+	# lkif_eq
+	triples, cardinality = hdt_kg.search_triples("", lkif_eq, "")
+	print ('lkif:eq ', cardinality)
 
 
 
@@ -78,15 +80,88 @@ print ('\t with ', len (entities), ' entities')
 # owl:sameAs
 triples, cardinality = hdt_kg.search_triples("", owl_sameas, "")
 print ('owl:sameAs ', cardinality)
+sameAs_entities = set()
+for (s, p, o) in triples:
+	print ('ow:sameAs: ',s,o)
+	sameAs_entities.add(s)
+	sameAs_entities.add(o)
+
+for c in sameAs_entities:
+	print ('*** ', c, ' ***')
+	triples, cardinality = hdt_kg.search_triples(c, "", "")
+	for (s, p, o) in triples:
+		print ('\t\tSAMEAS ',s, p,o)
+
+
+f = plt.figure()
+f.set_figwidth(5)
+f.set_figheight(3)
+barWidth = 0.33
+ax = plt.subplot(111)
+
 
 # skos:relatedMatch
 triples, cardinality = hdt_kg.search_triples("", skos_relatedMatch, "")
 print ('skos:relatedMatch ', cardinality)
 
+
+g_skos = nx.Graph()
+for (s, p, o) in triples:
+	g_skos.add_edge(s, o)
+
+# ccs = nx.connected_components(g_skos)
+# for cc in ccs:
+# 	if len (cc) > 13:
+# 		print('size = ',len (cc))
+# 		for c in cc:
+# 			print ('\t related_match: ', c)
+# 			triples, cardinality = hdt_kg.search_triples(c, "http://www.w3.org/2004/02/skos/core#prefLabel", "")
+# 			for (_, p, o) in triples:
+# 				print ('\t\t',p,o)
+
+ccs = nx.connected_components(g_skos)
+len_summary = [len (cc) for cc in ccs]
+ct = Counter(len_summary)
+print ('skos:relatedMatch connected components: ',ct)
+x = ct.keys()
+y = ct.values()
+ax.bar(x, y, color ='blue', width=barWidth, label='skos:relatedMatch', align='center')
+print ('skos_relatedMatch max ', max(ct.keys()))
+
 # skos:closeMatch
 triples, cardinality = hdt_kg.search_triples("", skos_closeMatch, "")
 print ('skos:closeMatch ', cardinality)
 
+g_skos = nx.Graph()
+for (s, p, o) in triples:
+	g_skos.add_edge(s, o)
+
+# ccs = nx.connected_components(g_skos)
+# for cc in ccs:
+# 	if len (cc) > 13:
+# 		print('size = ',len (cc))
+# 		for c in cc:
+# 			print ('\t close_match: ', c)
+# 			triples, cardinality = hdt_kg.search_triples(c, "http://www.w3.org/2004/02/skos/core#prefLabel", "")
+# 			for (_, p, o) in triples:
+# 				print ('\t\t',p,o)
+
+ccs = nx.connected_components(g_skos)
+len_summary = [len (cc) for cc in ccs]
+ct = Counter(len_summary)
+print ('skos_closeMatch connected components: ',ct)
+print ('skos_closeMatch max ', max(ct.keys()))
+
+# plot it out
+x = ct.keys()
+y = ct.values()
+x = [t + barWidth*1 for t in x]
+#
+# f = plt.figure()
+# f.set_figwidth(5)
+# f.set_figheight(1.1)
+# ax = plt.subplot(111)
+ax.bar(x, y, color ='red', width=barWidth, label = 'skos:closeMatch', align='center')
 
 # skos:exactMatch
 triples, cardinality = hdt_kg.search_triples("", skos_exactMatch, "")
@@ -100,7 +175,7 @@ for (s, p, o) in triples:
 ccs = nx.connected_components(g_skos)
 
 for cc in ccs:
-	if len (cc) >= 13:
+	if len (cc) > 13:
 		print('size = ',len (cc))
 		for c in cc:
 			print ('\t', c)
@@ -111,20 +186,22 @@ ccs = nx.connected_components(g_skos)
 len_summary = [len (cc) for cc in ccs]
 ct = Counter(len_summary)
 print ('connected components: ',ct)
-
+print ('skos:exactMatch max ', max(ct.keys()))
 
 # plot it out
 x = ct.keys()
 y = ct.values()
-
-f = plt.figure()
-f.set_figwidth(5)
-f.set_figheight(1.1)
-ax = plt.subplot(111)
-ax.bar(x, y, color ='grey', align='center')
+x = [t + barWidth*2 for t in x]
+#
+# f = plt.figure()
+# f.set_figwidth(5)
+# f.set_figheight(1.1)
+# ax = plt.subplot(111)
+ax.bar(x, y, color ='grey', width=barWidth, label = 'skos:exactMatch', align='center')
 ax.autoscale(tight=True)
-# ax.legend()
+ax.legend()
 plt.yscale('log')
+plt.xscale('log')
 plt.xlabel("size of connected components")
 plt.ylabel("frequency")
 ax.spines['top'].set_visible(False)
@@ -204,6 +281,11 @@ sccs = nx.strongly_connected_components(g_broader)
 len_summary = [len (scc) for scc in sccs]
 ct = Counter(len_summary)
 print ('strongly connected components: ',ct)
+
+
+# lkif_eq
+triples, cardinality = hdt_kg.search_triples("", lkif_eq, "")
+print ('lkif:eq ', cardinality)
 
 print ('\n-------INTEGRATED BROADER------\n')
 sccs = nx.strongly_connected_components(g_broader_integrated)
